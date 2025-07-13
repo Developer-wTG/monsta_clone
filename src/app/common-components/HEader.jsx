4de16151eb3c7f443a590e3403fb6bd78de1f6f0
Developer-wTG <unique.storme2009@gmail.com>
2025-07-13 21:11:00 +0530

    Card Layout

--- src/app/common-components/HEader.jsx
+++ src/app/common-components/HEader.jsx
@@ -1,16 +1,17 @@
+"use client"
 import Image from 'next/image'
 import React from 'react'
 import dummy from "../../../public/logo.png"
-
 import { CiSearch } from "react-icons/ci";
 import { IoMdCart } from "react-icons/io";
 import { FaHeart } from "react-icons/fa";
+
 const Header = () => {
     return (
         <>
-            <header className=''>
+            <header className=' hidden lg:block'>
                 {/* row-1 */}
-                <div className="border">
+                <div className="border hidden lg:block">
                     <section className='max-w-[1440px] flex justify-between py-4 mx-auto right1'>
                         <span className=' fontsninja-font'>Contact us 24/7 : +91-98745612330 / furnitureinfo@gmail.com</span>
                         <span className=' fontsninja-font endside'>Login / Register</span>
@@ -22,8 +23,8 @@ const Header = () => {
                         <figure className='w-[50%]  '>
                             <Image src={dummy} width={165} height={45} alt='logo' />
                         </figure>
-                        <aside className='w-[50%]  flex items-center justify-end right '>
-                            <input type="text" placeholder='Search product... ' className='w-[40%] border-[0.1vw] p-[10px] me-[3%]' /><CiSearch className='absolute top-23.5 right-110 w-[40px] h-[27px]' />
+                        <aside className='w-[50%]  flex items-center justify-end right relative '>
+                            <input type="text" placeholder='Search product... ' className='w-[40%] border-[0.1vw] p-[10px] me-[3%] hidden lg:block' /><CiSearch className='absolute hidden lg:block top-2.5 right-52 w-[40px] h-[27px]' />
                             <figure className='w-[5%] border  me-[3%] heart '>
                                 <FaHeart className='h-[43px] w-[30px] mx-auto  rounded' />
                                 
@@ -39,9 +40,9 @@ const Header = () => {
                     </section>
                 </div>
                 {/* row-3 */}
-                <section className='max-w-[1440px] flex  mx-auto '>
-                    <div className="flex mx-auto py-4 menu">
-                        <ul className='flex mx-[10px] items-center'>
+                <section className='max-w-[1440px]   mx-auto   hidden lg:block '>
+                    <div className="flex mx-auto py-4 menu ">
+                        <ul className='flex mx-[10px] items-center parent mx-auto '>
                             <li className=' mx-[30px] font-[Rubik] cursor-pointer p-[10px] text-[20px] text-[#C09578] cu ' >Home</li >
                             <li className=' mx-[30px] font-[Rubik] cursor-pointer p-[10px] text-[20px]' >Living
                                 <div className="megamenu">
@@ -109,13 +110,107 @@ const Header = () => {
                     </div>
 
                 </section>
+                
 
 
 
 
             </header>
+            <header >
+                    <section className=' responsive border  justify-between py-[26px] mx-auto   block lg:hidden flex '>
+                        <figure className='w-[30%]  border'>
+                            <Image src={dummy} width={165} height={45} alt='logo' />
+                        </figure>
+                         <figure className='w-[15%] border   heart '>
+                                <FaHeart className='h-[43px] w-[30px] mx-auto  rounded' />
+                                
+                            </figure>
+                        <div className="w-[15%] flex items-center border-[0.1vw] py-[4.5px] main text-center   ">
+                                <div className=""><IoMdCart className=' w-[40px] h-[35px] p-[5.5px] border-r-[0.1px] cart ' /></div>
+                                <p className='text-[18px] font-[Rubik Light] font-semibold pe-[5px]  '>Rs.000</p>
+
+                            </div>
+                        
+                        
+
+                    </section>
+                </header>
         </>
     )
 }
 
 export default Header
+
+// export function SwipeableTemporaryDrawer() {
+//   const [state, setState] = React.useState({
+//     top: false,
+//     left: false,
+//     bottom: false,
+//     right: false,
+//   });
+
+//   const toggleDrawer = (anchor, open) => (event) => {
+//     if (
+//       event &&
+//       event.type === 'keydown' &&
+//       (event.key === 'Tab' || event.key === 'Shift')
+//     ) {
+//       return;
+//     }
+
+//     setState({ ...state, [anchor]: open });
+//   };
+
+//   const list = (anchor) => (
+//     <Box
+//       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+//       role="presentation"
+//       onClick={toggleDrawer(anchor, false)}
+//       onKeyDown={toggleDrawer(anchor, false)}
+//     >
+//       <List>
+//         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+//           <ListItem key={text} disablePadding>
+//             <ListItemButton>
+//               <ListItemIcon>
+//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+//               </ListItemIcon>
+//               <ListItemText primary={text} />
+//             </ListItemButton>
+//           </ListItem>
+//         ))}
+//       </List>
+//       <Divider />
+//       <List>
+//         {['All mail', 'Trash', 'Spam'].map((text, index) => (
+//           <ListItem key={text} disablePadding>
+//             <ListItemButton>
+//               <ListItemIcon>
+//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+//               </ListItemIcon>
+//               <ListItemText primary={text} />
+//             </ListItemButton>
+//           </ListItem>
+//         ))}
+//       </List>
+//     </Box>
+//   );
+
+//   return (
+//     <div>
+//       {['left', 'right', 'top', 'bottom'].map((anchor) => (
+//         <React.Fragment key={anchor}>
+//           <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+//           <SwipeableDrawer
+//             anchor={anchor}
+//             open={state[anchor]}
+//             onClose={toggleDrawer(anchor, false)}
+//             onOpen={toggleDrawer(anchor, true)}
+//           >
+//             {list(anchor)}
+//           </SwipeableDrawer>
+//         </React.Fragment>
+//       ))}
+//     </div>
+//   );
+// }
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
 "use client";
 import Header from "./common-components/HEader";
-import React from "react";
+
+import React, { useState } from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import img1 from "../../public/imgi_35.jpg"
 import img2 from "../../public/imgi_36.jpg"
 import img3 from "../../public/imgi_3.webp"
 import img4 from "../../public/imgi_4.webp"
+import img5 from "../../public/imgi_6.jpg"
 import Image from "next/image";
+import { Container } from "@mui/material";
+import { FaHeart } from "react-icons/fa";
 
 
 
@@ -16,79 +20,108 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <Header/>
-    <Responsive/>
-    <div className="border-b-1">
-      <section className="max-w-[1440px] flex mx-auto py-5 ">
-        <div className="overflow-hidden relative w-[31.5%] me-[1.8%]">
-           <Image src={img3} width={null} height={null} alt="" className=" imagecss  "/>
-        </div>
-         <div className="overflow-hidden relative w-[31.5%] me-[1.8%]">
-           <Image src={img4} width={null} height={null} alt="" className=" imagecss "/>
-         </div>
-          <div className="overflow-hidden relative w-[31.5%] me-[1.8%]">
-            <Image src={img3} width={null} height={null} alt="" className=" imagecss "/>
+      <Header />
+      <SimpleSlider />
+      <div className="border-b-1">
+        <section className="max-w-[1440px] flex mx-auto py-[3rem] ">
+          <div className="overflow-hidden relative w-[31.5%] me-[1.8%] ">
+            <Image src={img3} width={null} height={null} alt="" className=" imagecss relative " />
+            <span className=" text-[22px] font-normal font-[Rubik, sans-serif] text-[#242424] absolute top-6 left-4.5 text-<base>/(27px) " >Design Creative</span>
+            <span className="text-[36px] font-bold font-sans  text-[#242424] absolute  top-14 left-4.5 text-<base>/(27px)">Chair Collection</span>
+            <div>
+
+
+
+            </div>
           </div>
-       
-       
-       
-        
+          <div className="overflow-hidden relative w-[31.5%] me-[1.8%]">
+            <Image src={img4} width={null} height={null} alt="" className=" imagecss relative " />
+            <div>
+              <span className=" text-[22px] font-normal font-[Rubik, sans-serif] text-[#242424] absolute top-6 left-4.5 text-<base>/(27px)" >
+                Bestselling Products</span>
 
-      </section>
+              <span className="text-[36px] font-bold font-sans  text-[#242424] absolute top-14 left-4.5 text-<base>/(27px)">Chair Collection</span>
+            </div>
+          </div>
+          <div className="overflow-hidden relative w-[31.5%] me-[1.8%]">
+            <Image src={img3} width={null} height={null} alt="" className=" imagecss relative " />
+            <div>
+              <span className=" text-[22px] font-normal font-[Rubik, sans-serif] text-[#242424] absolute top-6 left-4.5 text-<base>/(27px)" >Onsale Products</span>
 
-    </div>
+              <span className="text-[36px] font-bold font-sans  text-[#242424] absolute top-14 left-4.5 text-<base>/(27px)">Chair Collection</span>
+            </div>
+          </div>
+
+
+
+
+
+        </section>
+
+      </div>
+      <div className="max-w-[1440px] mx-auto  flex mt-[4rem] mb-[3rem] flex-wrap">
+        <aside className="flex mx-auto border w-full">
+          <div className="mx-auto">
+            <button className="text-[2rem]  border-2 font-serif px-[8px] font-[55] hover:text-[#C0958B] " >Featured</button>
+            <button className="text-[2rem]  border-y-2 font-serif px-[8px] font-[55] hover:text-[#C0958B] ">New Arrivals</button>
+            <button className="text-[2rem]  border-2 font-serif px-[8px] font-[55] hover:text-[#C0958B]">Onsale</button>
+          </div>
+        </aside>
+
+        <aside className="my-8 card-section flex flex-col items-center justify-center p-6 shadow-2xl bg-white max-w-[360px] mx-auto font-['Rubik',sans-serif] transition-transform duration-300 hover:scale-105">
+          <div className="w-full">
+            <Image src={img5} width={0} height={0} alt="Product Image" className=" object-cover w-full h-auto" />
+            <p className="text-center py-2 text-[16px] font-semibold text-gray-800">Nest Of Tables</p>
+            <p className="text-center border-b font-bold text-[20px] pb-1 text-[#1a1a1a]">Caroline Study Tables</p>
+            <div className="text-center flex justify-center items-center gap-4 py-3">
+              <span className="line-through text-gray-500 text-[18px]">Rs.3000</span>
+              <span className="text-[#C0958B] font-bold text-[20px]">Rs.2500</span>
+            </div>
+          </div>
+          <div className="py-3 flex items-center gap-4 mt-2">
+            <FaHeart className="h-[40px] w-[40px] p-2 bg-[#ccc]  hover:bg-red-400 transition" />
+            <p className="text-[18px] px-4 py-1 border  bg-[#ccc]] hover:bg-[#C0958B] hover:text-white cursor-pointer transition-all">
+              Add To Cart
+            </p>
+          </div>
+        </aside>
+
+      </div>
 
     </>
   );
 }
-function Responsive() {
-  var settings = {
+function SimpleSlider() {
+  const settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    slidesToScroll: 1
   };
   return (
-    <div className="slider-container">
-      <Slider {...settings} className="custom-dots-slider">
-        <div>
-          <Image src={img1} width={null} height={null}/>
-        </div>
-        <div>
-          <Image src={img2} width={null} height={null}/>
-        </div>
-        
-      </Slider>
+
+    <div className=" p-0 m-0 ">
+      <div className=" overflow-hidden  p-0 m-0   ">
+        <Slider {...settings} className="custom-dots-slider  p-0 m-0">
+          <div>
+            <Image src={img1} width={null} height={null} className=" w-full " />
+          </div>
+          <div>
+            <Image src={img2} width={null} height={null} className="w-full  " />
+          </div>
+
+        </Slider>
+      </div>
     </div>
+
   );
 }
 
-export  {Responsive};
+export { SimpleSlider };
+
+
+
+
+
+
